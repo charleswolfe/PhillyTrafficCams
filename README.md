9f3c07e10ff425676c8a9b5e18388132bba0bbc4
Charles Wolfe <charleswolfe@gajo.us>
2018-11-30 15:28:59 -0500

    add hls lib

--- README.md
+++ README.md
@@ -1,15 +1,27 @@
-# PhillyTrafficCams
-<<<<<<< HEAD
+PhillyTrafficCams
 =======
 
 
-get key
-http://www.511pa.com/wowzKey.aspx
+
 
 camera_stream_timeout_seconds is set to 60
 
+The real video page:
+https://www.511pa.com/flowplayeri.aspx?CAMID=CAM-06-012
 
+
+The source files, not sure if I will need
 http://pa511wmedia101.ilchost.com/live/CAM-06-440.stream/media_w1240356806_22359.ts?wmsAuthSign=xxx
-
 http://pa511wmedia101.ilchost.com/live/CAM-06-337.stream/media_w1478814025_7193.ts?wmsAuthSign=xxx
->>>>>>> 9e0237d23b1ce9a57d9bbe3f7901d15d0109f7a5
+
+
+Actual urls being hit
+
+https://pa511wmedia100.ilchost.com/live/CAM-06-012.stream/chunklist_w1210102094.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAxOCA3OjExOjA0IFBNJmhhc2hfdmFsdWU9bEFVaHhrTnh6UkJFV0R5dERzdndOdz09JnZhbGlkbWludXRlcz0yMCZpZD03NS4xNTEuMTY1LjIyMQ%3D%3D
+
+then we hit
+
+https://pa511wmedia100.ilchost.com/live/CAM-06-012.stream/media_w1210102094_2573.ts?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAxOCA3OjExOjA0IFBNJmhhc2hfdmFsdWU9bEFVaHhrTnh6UkJFV0R5dERzdndOdz09JnZhbGlkbWludXRlcz0yMCZpZD03NS4xNTEuMTY1LjIyMQ%3D%3D
+
+
+url + cam# + media/chunklist + wId + (optional frame id if media) + authkey
--- trafficcams/src/app/app.component.ts
+++ trafficcams/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpClientModule } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  mainUrl = 'https://www.511pa.com/';
+  paVideoUrl = '//pa511wmedia101.ilchost.com/live/';
+  wmsAuthUrl = 'http://www.511pa.com/wowzKey.aspx';
+
+  videoSrc = '';
+  videoSourceSrc = '';
+  wmsAuthSign = '';
+
+
+
+  constructor(private httpClient:HttpClient) {
+
+
+      this.getWmsAuth();
+
+      //the follwing is not need now
+      //set cookies for AWSSESSION_ID
+      //  and CAMERAVIEWTYPE = video
+      //  and AWSUSER_ID
+
+
+      //then have a (local?) list of camera ids
+
+      //urls in configs, probably
+
+
+      //we need to start pulling the video and figure out 9e18858a-158e-4d00-98f7-83b9c81e4e0d
+
+
+  }
+
+    togglePlayPause() {
+      alert('you suck');
+    }
+
+    getWmsAuth() {
+        this.httpClient
+            .get(this.wmsAuthUrl, {responseType: 'text'})
+            .subscribe(data => {
+                console.log(data);
+                this.wmsAuthSign = data;
+                this.setVideoSrcUrl(null);
+            });
+    }
+
+    //just a part of setup
+    setVideoSrcUrl(cam) {
+      if (!cam) {
+        cam = 'CAM-01-008';
+      }
+        this.videoSourceSrc = this.paVideoUrl + cam + '.stream/playlist.m3u8?wmsAuthSign=' + this.wmsAuthSign;
+        this.videoSrc = 'blob:' + this.mainUrl + '9e18858a-158e-4d00-98f7-83b9c81e4e0d';
+    }
 }
